db3521e9ad9c0fe64d3e38d7c87d2099577144a4
William Yang <96196310+DaemonCypher@users.noreply.github.com>
2023-07-19 15:06:31 -0500

    Update property.py

--- Monopoly/property.py
+++ Monopoly/property.py
@@ -1,49 +1,63 @@
+'''
+cost: cost of property 
+name: name of property
+1house: rent for 1 house upgrade on property
+2house: rent for 2 house upgrade on property
+3house: rent for 3 house upgrade on property
+4house: rent for 4 house upgrade on property
+Hotel: rent for hotel upgrade on property
+color: color of property. Helps identify set benfits of properties
+housecost: cost of upgrade of property
+updgrade: number of upgrades on property
+mortageValue: value of mortaging the property
+owner: owner of the property
+mortgage: mortgage status
+'''
 property_data = {
-	1 : {'cost': 60, 'name': 'Mediterranean Avenue','1House': 10,'2House': 30,'3House': 90,'4House':160, 'Hotel':250, 'color': 'purple','houseCost':50,'upgrade':0,'mortageValue':30, 'owner': 'Bank'},
-	3 : {'cost': 60, 'name': 'Baltic Avenue','1House': 20,'2House': 60,'3House': 180,'4House':320, 'Hotel':450, 'color': 'purple', 'houseCost':50,'upgrade':0,'mortageValue':30, 'owner': 'Bank'},
+	1 : {'cost': 60, 'name': 'Mediterranean Avenue','1House': 10,'2House': 30,'3House': 90,'4House':160, 'Hotel':250, 'color': 'purple','houseCost':50,'upgrade':0,'mortageValue':30, 'owner': 'Bank', 'mortgage': False},
+	3 : {'cost': 60, 'name': 'Baltic Avenue','1House': 20,'2House': 60,'3House': 180,'4House':320, 'Hotel':450, 'color': 'purple', 'houseCost':50,'upgrade':0,'mortageValue':30, 'owner': 'Bank','mortgage': False},
 
-    6 : {'cost': 100, 'name': 'Oriental Avenue','1House': 30,'2House': 90,'3House': 270,'4House':400, 'Hotel':550, 'color': 'light blue', 'houseCost':50,'upgrade':0,'mortageValue':50, 'owner': 'Bank'},
-    8 : {'cost': 100, 'name': 'Vermont Avenue','1House': 30,'2House': 90,'3House': 270,'4House':400, 'Hotel':550, 'color': 'light blue', 'houseCost':50,'upgrade':0,'mortageValue':50, 'owner': 'Bank'},
-    9 : {'cost': 120, 'name': 'Connecticut Avenue','1House': 40,'2House': 100,'3House': 300,'4House':450, 'Hotel':600,'color': 'lightblue','houseCost':50,'upgrade':0,'mortageValue':60, 'owner': 'Bank'},
+    6 : {'cost': 100, 'name': 'Oriental Avenue','1House': 30,'2House': 90,'3House': 270,'4House':400, 'Hotel':550, 'color': 'light blue', 'houseCost':50,'upgrade':0,'mortageValue':50, 'owner': 'Bank','mortgage': False},
+    8 : {'cost': 100, 'name': 'Vermont Avenue','1House': 30,'2House': 90,'3House': 270,'4House':400, 'Hotel':550, 'color': 'light blue', 'houseCost':50,'upgrade':0,'mortageValue':50, 'owner': 'Bank','mortgage': False},
+    9 : {'cost': 120, 'name': 'Connecticut Avenue','1House': 40,'2House': 100,'3House': 300,'4House':450, 'Hotel':600,'color': 'lightblue','houseCost':50,'upgrade':0,'mortageValue':60, 'owner': 'Bank', 'mortgage': False},
 
-    11 : {'cost': 140, 'name': 'St. Charles Place','1House': 50,'2House': 150,'3House': 450,'4House':625, 'Hotel':750,'color': 'pink','houseCost':100,'upgrade':0,'mortageValue':70, 'owner': 'Bank'},
-    13 : {'cost': 140, 'name': 'States Avenue','1House': 50,'2House': 150,'3House': 450,'4House':625, 'Hotel':750,'color': 'pink','houseCost':100,'upgrade':0,'mortageValue':70, 'owner': 'Bank'},
-    14 : {'cost': 160, 'name': 'Virginia Avenue','1House': 60,'2House': 180,'3House': 500,'4House':700, 'Hotel':900,'color': 'pink','houseCost':100,'upgrade':0,'mortageValue':80, 'owner': 'Bank'},
+    11 : {'cost': 140, 'name': 'St. Charles Place','1House': 50,'2House': 150,'3House': 450,'4House':625, 'Hotel':750,'color': 'pink','houseCost':100,'upgrade':0,'mortageValue':70, 'owner': 'Bank','mortgage': False},
+    13 : {'cost': 140, 'name': 'States Avenue','1House': 50,'2House': 150,'3House': 450,'4House':625, 'Hotel':750,'color': 'pink','houseCost':100,'upgrade':0,'mortageValue':70, 'owner': 'Bank','mortgage': False},
+    14 : {'cost': 160, 'name': 'Virginia Avenue','1House': 60,'2House': 180,'3House': 500,'4House':700, 'Hotel':900,'color': 'pink','houseCost':100,'upgrade':0,'mortageValue':80, 'owner': 'Bank', 'mortgage': False},
 
-    16 : {'cost': 180, 'name': 'St. James Place','1House': 70,'2House': 200,'3House': 550,'4House':750, 'Hotel':950,'color':'orange','houseCost':100,'upgrade':0,'mortageValue':90, 'owner': 'Bank'},
-    18 : {'cost': 180, 'name': 'Tennessee Avenue','1House': 70,'2House': 200,'3House': 550,'4House':750, 'Hotel':950,'color':'orange','houseCost':100,'upgrade':0,'mortageValue':90, 'owner': 'Bank'},
-    19 : {'cost': 200, 'name': 'New York Avenue','1House': 80,'2House': 220,'3House': 600,'4House':800, 'Hotel':100,'color':'orange','houseCost':100,'upgrade':0,'mortageValue':100, 'owner': 'Bank'},
+    16 : {'cost': 180, 'name': 'St. James Place','1House': 70,'2House': 200,'3House': 550,'4House':750, 'Hotel':950,'color':'orange','houseCost':100,'upgrade':0,'mortageValue':90, 'owner': 'Bank','mortgage': False},
+    18 : {'cost': 180, 'name': 'Tennessee Avenue','1House': 70,'2House': 200,'3House': 550,'4House':750, 'Hotel':950,'color':'orange','houseCost':100,'upgrade':0,'mortageValue':90, 'owner': 'Bank','mortgage': False},
+    19 : {'cost': 200, 'name': 'New York Avenue','1House': 80,'2House': 220,'3House': 600,'4House':800, 'Hotel':100,'color':'orange','houseCost':100,'upgrade':0,'mortageValue':100, 'owner': 'Bank','mortgage': False},
 
-    21 : {'cost': 220, 'name': 'Kentucky Avenue','1House': 90,'2House': 250,'3House': 700,'4House':870, 'Hotel':1050,'color':'red','houseCost':150,'upgrade':0,'mortageValue':110, 'owner': 'Bank'},
-    23 : {'cost': 220, 'name': 'Indiana Avenue','1House': 90,'2House': 250,'3House': 700,'4House':870, 'Hotel':1050,'color':'red','houseCost':150,'upgrade':0,'mortageValue':110, 'owner': 'Bank'},
-    24 : {'cost': 240, 'name': 'Illinois Avenue','1House': 100,'2House': 300,'3House': 750,'4House':925, 'Hotel':1100,'color':'red','houseCost':150,'upgrade':0,'mortageValue':120, 'owner': 'Bank'},
+    21 : {'cost': 220, 'name': 'Kentucky Avenue','1House': 90,'2House': 250,'3House': 700,'4House':870, 'Hotel':1050,'color':'red','houseCost':150,'upgrade':0,'mortageValue':110, 'owner': 'Bank','mortgage': False},
+    23 : {'cost': 220, 'name': 'Indiana Avenue','1House': 90,'2House': 250,'3House': 700,'4House':870, 'Hotel':1050,'color':'red','houseCost':150,'upgrade':0,'mortageValue':110, 'owner': 'Bank','mortgage': False},
+    24 : {'cost': 240, 'name': 'Illinois Avenue','1House': 100,'2House': 300,'3House': 750,'4House':925, 'Hotel':1100,'color':'red','houseCost':150,'upgrade':0,'mortageValue':120, 'owner': 'Bank','mortgage': False},
 
-    26 : {'cost': 260, 'name': 'Atlantic Avenue','1House': 110,'2House': 330,'3House': 800,'4House':975, 'Hotel':1150,'color':'yellow','houseCost':150,'upgrade':0,'mortageValue':130, 'owner': 'Bank'},
-    27 : {'cost': 260, 'name': 'Ventnor Avenue','1House': 110,'2House': 330,'3House': 800,'4House':975, 'Hotel':1150,'color':'yellow','houseCost':150,'upgrade':0,'mortageValue':130, 'owner': 'Bank'},
-    29 : {'cost': 280, 'name': 'Marvin Gardens','1House': 120,'2House': 360,'3House': 850,'4House':1025, 'Hotel':1200,'color':'yellow','houseCost':150,'upgrade':0,'mortageValue':140, 'owner': 'Bank'},
+    26 : {'cost': 260, 'name': 'Atlantic Avenue','1House': 110,'2House': 330,'3House': 800,'4House':975, 'Hotel':1150,'color':'yellow','houseCost':150,'upgrade':0,'mortageValue':130, 'owner': 'Bank','mortgage': False},
+    27 : {'cost': 260, 'name': 'Ventnor Avenue','1House': 110,'2House': 330,'3House': 800,'4House':975, 'Hotel':1150,'color':'yellow','houseCost':150,'upgrade':0,'mortageValue':130, 'owner': 'Bank','mortgage': False},
+    29 : {'cost': 280, 'name': 'Marvin Gardens','1House': 120,'2House': 360,'3House': 850,'4House':1025, 'Hotel':1200,'color':'yellow','houseCost':150,'upgrade':0,'mortageValue':140, 'owner': 'Bank','mortgage': False},
 
-    31 : {'cost': 300, 'name': 'Pacific Avenue','1House': 130,'2House': 390,'3House': 900,'4House':1100, 'Hotel':1275,'color':'green','houseCost':200,'upgrade':0,'mortageValue':150, 'owner': 'Bank'},
-    32 : {'cost': 300, 'name': 'North Carolina Avenue','1House': 130,'2House': 390,'3House': 900,'4House':1100, 'Hotel':1275,'color':'green','houseCost':200,'upgrade':0,'mortageValue':150, 'owner': 'Bank'},
-    34 : {'cost': 320, 'name': 'Pennsylvania Avenue','1House': 150,'2House': 450,'3House': 1000,'4House':1200, 'Hotel':1400,'color':'green','houseCost':200,'upgrade':0,'mortageValue':160, 'owner': 'Bank'},
+    31 : {'cost': 300, 'name': 'Pacific Avenue','1House': 130,'2House': 390,'3House': 900,'4House':1100, 'Hotel':1275,'color':'green','houseCost':200,'upgrade':0,'mortageValue':150, 'owner': 'Bank','mortgage': False},
+    32 : {'cost': 300, 'name': 'North Carolina Avenue','1House': 130,'2House': 390,'3House': 900,'4House':1100, 'Hotel':1275,'color':'green','houseCost':200,'upgrade':0,'mortageValue':150, 'owner': 'Bank','mortgage': False},
+    34 : {'cost': 320, 'name': 'Pennsylvania Avenue','1House': 150,'2House': 450,'3House': 1000,'4House':1200, 'Hotel':1400,'color':'green','houseCost':200,'upgrade':0,'mortageValue':160, 'owner': 'Bank','mortgage': False},
 
-    37 : {'cost': 350, 'name': 'Park Place','1House': 175,'2House': 500,'3House': 1100,'4House':1300, 'Hotel':1500,'color':'blue','houseCost':200,'upgrade':0,'mortageValue':175, 'owner': 'Bank'},
-    39 : {'cost': 400, 'name': 'Boardwalk','1House': 200,'2House': 600,'3House': 1400,'4House':1700, 'Hotel':200,'color':'blue','houseCost':200,'upgrade':0,'mortageValue':200, 'owner': 'Bank'},
+    37 : {'cost': 350, 'name': 'Park Place','1House': 175,'2House': 500,'3House': 1100,'4House':1300, 'Hotel':1500,'color':'blue','houseCost':200,'upgrade':0,'mortageValue':175, 'owner': 'Bank','mortgage': False},
+    39 : {'cost': 400, 'name': 'Boardwalk','1House': 200,'2House': 600,'3House': 1400,'4House':1700, 'Hotel':200,'color':'blue','houseCost':200,'upgrade':0,'mortageValue':200, 'owner': 'Bank','mortgage': False},
 }
-# TODO need to change railroad card values remove house cost, upgrade, hotel
+
 railroad_data = {
 
-    5 : {'cost': 200, 'name': 'Reading Railroad','1House': 25,'2House': 50,'3House': 100,'4House':200, 'Hotel':0, 'color': 'black', 'houseCost':0,'upgrade':0,'mortageValue':100, 'owner': 'Bank'},
-    15 : {'cost': 200, 'name': 'Pennsylvania Railroad','1House': 25,'2House': 50,'3House': 100,'4House':200, 'Hotel':0, 'color': 'black', 'houseCost':50,'upgrade':0,'mortageValue':100, 'owner': 'Bank'},
-    25 : {'cost': 200, 'name': 'B. & O. Railroad','1House': 25,'2House': 50,'3House': 100,'4House':200, 'Hotel':0,'color': 'black','houseCost':50,'upgrade':0,'mortageValue':100, 'owner': 'Bank'},
-    35 : {'cost': 200, 'name': 'Short Line','1House': 25,'2House': 50,'3House': 100,'4House':200, 'Hotel':0,'color': 'black','houseCost':50,'upgrade':0,'mortageValue':100, 'owner': 'Bank'},
+    5 : {'cost': 200, 'name': 'Reading Railroad', 'color': 'black','mortageValue':100, 'owner': 'Bank','mortgage': False},
+    15 : {'cost': 200, 'name': 'Pennsylvania Railroad','color': 'black','mortageValue':100, 'owner': 'Bank','mortgage': False},
+    25 : {'cost': 200, 'name': 'B. & O. Railroad','color': 'black','mortageValue':100, 'owner': 'Bank','mortgage': False},
+    35 : {'cost': 200, 'name': 'Short Line','color': 'black','mortageValue':100, 'owner': 'Bank','mortgage': False},
 }
-# TODO need to change utility card values
+
 utility_data = {
-
-    12 : {'cost': 150, 'name': 'Electric Company','1House': 175,'2House': 500,'3House': 1100,'4House':1300, 'Hotel':1500,'color':'white','houseCost':200,'upgrade':0,'mortageValue':75, 'owner': 'Bank'},
-    28 : {'cost': 150, 'name': 'Water Works','1House': 200,'2House': 600,'3House': 1400,'4House':1700, 'Hotel':200,'color':'white','houseCost':200,'upgrade':0,'mortageValue':75, 'owner': 'Bank'},
-
+    12 : {'cost': 150, 'name': 'Electric Company','color':'white','mortageValue':75, 'owner': 'Bank','mortgage': False},
+    28 : {'cost': 150, 'name': 'Water Works','color':'white','mortageValue':75, 'owner': 'Bank','mortgage': False},
 }
+
 special_tiles = {
 
     2 :{'name': "community chest 1"},
@@ -61,6 +75,7 @@ special_tiles = {
 
 
 }
+
 chance_card = {
 
     1:{'name':'Advance to Boardwalk'},
